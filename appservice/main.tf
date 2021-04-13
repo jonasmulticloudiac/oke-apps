010a292b089114cf28f0e91422fa89aad1d86605
root <root@DESKTOP-S7GGDE6.localdomain>
2021-04-13 11:56:58 -0300

    Start project

--- appservice/main.tf
+++ appservice/main.tf
@@ -1,3 +1,10 @@
+data "terraform_remote_state" "proj" {
+  backend = "http"
+  config {
+    address = "https://objectstorage.oraclecloud.com/p/5Rlj6x80V7aJML__7aypnwhjdaIEU9VmcYMj_96WZjw_F1xxZPSIZGN-srdV8Zq3/n/gr6yn2oixrh2/b/terraform-okeapps/o/terraform.tfstate"
+  }
+}
+
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
