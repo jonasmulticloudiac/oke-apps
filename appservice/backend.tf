9356b9408466a65d114eb966c19bffab6c4d7fd0
root <root@DESKTOP-S7GGDE6.localdomain>
2021-04-13 10:00:39 -0300

    Start the project

--- appservice/backend.tf
+++ appservice/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "http" {
     update_method = "PUT"
-    address       = "https://objectstorage.sa-saopaulo-1.oraclecloud.com/p/5Rlj6x80V7aJML__7aypnwhjdaIEU9VmcYMj_96WZjw_F1xxZPSIZGN-srdV8Zq3/n/gr6yn2oixrh2/b/terraform-okeapps/o/terraform.tfstate"
+    address       = "https://objectstorage.${var.tf_state}"
   }
 }
 
